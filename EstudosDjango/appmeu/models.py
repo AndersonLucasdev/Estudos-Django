9db704433b5c1db5445acb08db168c2a1cc29edf
anderson <andinlucas8@gmail.com>
2024-01-07 15:29:39 -0300

    feat/ startapp: Adicionando migrate

---
+++ EstudosDjango/appmeu/models.py
@@ -0,0 +1,9 @@
+# models.py
+from django.db import models
+
+class Item(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
