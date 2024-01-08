3d472f287e86627d10f1a9a9576f266e6ebc2046
anderson <andinlucas8@gmail.com>
2024-01-08 14:38:48 -0300

    REFACTOR/ urls: Modificando urls em EstudosDjango

--- EstudosDjango/appmeu/urls.py
+++ EstudosDjango/appmeu/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('lista/', views.lista_itens, name='lista_itens'),
+    path('cria/', views.cria_item, name='cria_item'),
+    path('atualiza/<int:item_id>/', views.atualiza_item, name='atualiza_item'),
+    path('exclui/<int:item_id>/', views.exclui_item, name='exclui_item'),
+]
