ff4bf1089d35ed5995902ef363cf32a6a97e3700
anderson <andinlucas8@gmail.com>
2024-01-08 14:33:24 -0300

    feat/ views: Começando views

--- EstudosDjango/appmeu/views.py
+++ EstudosDjango/appmeu/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
