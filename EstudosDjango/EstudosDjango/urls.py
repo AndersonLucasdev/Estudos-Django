9e97d29ab0f535ac5d687f417929260470d8f132
anderson <andinlucas8@gmail.com>
2024-01-09 15:15:25 -0300

    fix/ urls: Corrigindo erro na pasta app

--- EstudosDjango/EstudosDjango/urls.py
+++ EstudosDjango/EstudosDjango/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meuapp/', include('meuapp.urls')),
+    path('appmeu/', include('appmeu.urls')),
 ]
