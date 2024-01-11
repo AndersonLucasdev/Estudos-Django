4d25852ac91108b5461fdbfe2fefdd7dabb7d156
anderson <andinlucas8@gmail.com>
2024-01-11 11:05:37 -0300

    refactor/ models: Iniciando model tarefa

--- EstudosDjango/appmeu/models.py
+++ EstudosDjango/appmeu/models.py
@@ -1,9 +1,11 @@
 # models.py
 from django.db import models
 
-class Item(models.Model):
-    nome = models.CharField(max_length=255)
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200)
     descricao = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.nome
+        return self.titulo
