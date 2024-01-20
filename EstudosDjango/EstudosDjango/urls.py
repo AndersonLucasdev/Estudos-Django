5f586d8c71ebeb89b49d0adbf267d15f3724e323
anderson <andinlucas8@gmail.com>
2024-01-20 09:57:46 -0300

    feat/ urls: Adicionando urls de auth

--- EstudosDjango/EstudosDjango/urls.py
+++ EstudosDjango/EstudosDjango/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
-from EstudosDjango.appmeu import views as appmeu_views 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('appmeu/', include('gerenciador_tarefas.appmeu.urls')),  
-    path('login/', auth_views.LoginView.as_view(template_name='tarefas/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('registro/', appmeu_views.registro, name='registro'), 
 ]
--- EstudosDjango/appmeu/urls.py
+++ EstudosDjango/appmeu/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('cria/', views.cria_item, name='cria_item'),
     path('atualiza/<int:item_id>/', views.atualiza_item, name='atualiza_item'),
     path('exclui/<int:item_id>/', views.exclui_item, name='exclui_item'),
-    path('login/', auth_views.LoginView.as_view(template_name='tarefas/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='tarefas/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('registro/', views.registro, name='registro'),
 ]
