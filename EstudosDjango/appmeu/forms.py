9baefb59cbf0f6a8c27d6f9104422de168e162a7
anderson <andinlucas8@gmail.com>
2024-01-21 22:17:09 -0300

    feat/ forms: Criando CadastroForm

--- EstudosDjango/appmeu/forms.py
+++ EstudosDjango/appmeu/forms.py
@@ -7,3 +7,13 @@ class TarefaForm(forms.ModelForm):
     class Meta:
         model = Tarefa
         fields = '__all__'
+
+class CadastroForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    # Adicione os campos adicionais
+    # campo_adicional = forms.CharField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
