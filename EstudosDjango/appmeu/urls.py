d861bfb3aa3664768cdeed3b033df2064360463d
anderson <andinlucas8@gmail.com>
2024-01-25 20:50:34 -0300

    feat/ views: Adicionando na url

--- EstudosDjango/appmeu/urls.py
+++ EstudosDjango/appmeu/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('exclui/<int:item_id>/', views.exclui_item, name='exclui_item'),
     path('login/', views.LoginView.as_view(template_name='tarefas/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('tarefa/<int:tarefa_id>/conclui/', views.marca_tarefa_concluida, name='marca_tarefa_concluida'),
     path('registro/', views.registro, name='registro'),
 ]
