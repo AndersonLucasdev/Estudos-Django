3935b72b855be7570502dbbd24d9ca431d70dc6d
anderson <andinlucas8@gmail.com>
2024-01-30 20:51:24 -0300

    feat/ models: Finalizando implementação de comentários

--- EstudosDjango/appmeu/models.py
+++ EstudosDjango/appmeu/models.py
@@ -16,3 +16,7 @@ class Comentario(models.Model):
     tarefa = models.ForeignKey('Tarefa', on_delete=models.CASCADE, related_name='comentarios')
     texto = models.TextField()
     data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comentário por {self.autor.username} em {self.data_criacao}'
+    
