ae6785f161cce781f71982697489d5ac678d50c4
anderson <andinlucas8@gmail.com>
2024-01-30 20:49:44 -0300

    feat/ models: Adicionando comentários

--- EstudosDjango/appmeu/models.py
+++ EstudosDjango/appmeu/models.py
@@ -1,5 +1,6 @@
 # models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=200)
@@ -9,3 +10,9 @@ class Tarefa(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class Comentario(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    tarefa = models.ForeignKey('Tarefa', on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
