974584e6cf71ca0f1aca0de3211f4da1a416cc50
anderson <andinlucas8@gmail.com>
2024-01-31 21:52:47 -0300

    feat/ forms: Adicionando comentário

--- EstudosDjango/appmeu/forms.py
+++ EstudosDjango/appmeu/forms.py
@@ -17,3 +17,10 @@ class CadastroForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+from .models import Comentario
+
+class ComentarioForm(forms.ModelForm):
+    class Meta:
+        model = Comentario
+        fields = ['texto']
