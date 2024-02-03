7d866736301600e3cc0c8560a491eca399945cbd
anderson <andinlucas8@gmail.com>
2024-02-02 21:10:56 -0300

    feat/ models: Adicionando fav, prioridade e user

--- EstudosDjango/appmeu/models.py
+++ EstudosDjango/appmeu/models.py
@@ -3,10 +3,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tarefa(models.Model):
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
     descricao = models.TextField()
-    data_criacao = models.DateTimeField(auto_now_add=True)
+    prazo = models.DateField()
     concluida = models.BooleanField(default=False)
+    favorito = models.BooleanField(default=False)
+    prioridade = models.BooleanField(default=False)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
